f8450e0c1b910f91d792ad3f54cadb858c1640f0
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-11-23 21:05:48 +0000

    Update automated-notebook-run-script.py

--- scripts/automated-notebook-run-script.py
+++ scripts/automated-notebook-run-script.py
@@ -3,22 +3,31 @@ import pyautogui
 import time
 
 def main():
+    # Click Safari icon
     pyautogui.moveTo(150, 720, duration=1)
     pyautogui.click()
     time.sleep(1)
+    # Click Safari Menu
     pyautogui.moveTo(60, 10, duration=1)
     pyautogui.click()
     time.sleep(1)
+    # Click Settings
     pyautogui.moveTo(75, 102, duration=1)
     pyautogui.click()
     time.sleep(1.2)
+    # This is done while waiting for settings page
+    # to appear
     pyautogui.moveTo(10, 20, duration=1)
     time.sleep(1.2)
+    # Click websites page of settings
     pyautogui.moveTo(700, 240, duration=1)
     pyautogui.click()
     time.sleep(1.2)
+    # Click Downloads section of webpages page
     pyautogui.moveTo(350, 630, duration=1)
     pyautogui.click()
+    time.sleep(1.2)
+    pyautogui.moveTo(800, 670, duration=1)
     time.sleep(1.2)
     output_file = "screenshot.png"
     subprocess.run(["screencapture", "-C", output_file])
