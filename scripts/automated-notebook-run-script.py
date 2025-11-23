71ff47749c7bdfe5ddc908a34cf20f98aa9b6e16
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-11-23 20:09:17 +0000

    Update automated-notebook-run-script.py

--- scripts/automated-notebook-run-script.py
+++ scripts/automated-notebook-run-script.py
@@ -9,7 +9,7 @@ def main():
     pyautogui.moveTo(60, 10, duration=1)
     pyautogui.click()
     time.sleep(1)
-    pyautogui.moveTo(60, 50, duration=1)
+    pyautogui.moveTo(60, 180, duration=1)
     pyautogui.click()
     time.sleep(1)
     output_file = "screenshot.png"
